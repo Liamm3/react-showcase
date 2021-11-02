c9dcce4333872b4ed81e34aa8b1f91c0810831f5
Liamm3 <liam.wohlfart0@gmail.com>
2021-11-02 22:47:43 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Switch, Route } from 'react-router-dom'
+
 import Person from './containers/Person'
 import Navbar from './components/Navbar'
 
 const App = () => (
   <>
     <Navbar />
+    <Switch>
+      <Route path="/" component={Person} />
+    </Switch>
   </>
 )
 
