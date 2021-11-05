b69758374534fd2bc4c08d1e46158fa4c8a7a8af
Liamm3 <liam.wohlfart0@gmail.com>
2021-11-05 09:38:42 +0100

    move sidebar to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import { Switch, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 import Users from './containers/Users'
 import Navbar from './components/Navbar'
 import Home from './containers/Home'
+import Sidebar from './components/Sidebar'
 
-const App = () => (
-  <>
-    <Navbar />
-    <Switch>
-      <Route path="/users" component={Users} />
-      <Route path="/" component={Home} />
-    </Switch>
-  </>
-)
+function App() {
+  const [drawerOpen, setDrawerOpen] = useState()
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen)
+  }
+
+  return (
+    <>
+      <Navbar toggleDrawer={toggleDrawer} />
+      <Sidebar toggleDrawer={toggleDrawer} drawerOpen={drawerOpen} />
+      <Switch>
+        <Route path="/users" component={Users} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </>
+  )
+}
 
 export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
@@ -6,37 +5,26 @@ import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 
-import Sidebar from './Sidebar'
-
-export default function Navbar() {
-  const [drawerOpen, setDrawerOpen] = useState()
-
-  const toggleDrawer = () => {
-    setDrawerOpen(!drawerOpen)
-  }
-
+export default function Navbar({ toggleDrawer }) {
   return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="sticky">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={toggleDrawer}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              React Showcase
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Sidebar drawerOpen={drawerOpen} toggleDrawer={toggleDrawer} />
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React Showcase
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
