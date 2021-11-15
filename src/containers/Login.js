208d7d9c7c46ed8510bf2e40c391c1ba6684bdf3
Liamm3 <liam.wohlfart0@gmail.com>
2021-11-15 15:46:51 +0100

    Add login formular

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,57 @@
-import { Typography } from '@mui/material'
-import { Container } from '@mui/material'
+import { Grid, TextField, Typography, Button } from '@mui/material'
+import { useState } from 'react'
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    //
+  }
+
   return (
-    <Container>
-      <Typography variant="h2">Login</Typography>
-    </Container>
+    <>
+      <Typography
+        variant="h2"
+        sx={{
+          marginBottom: 3
+        }}
+      >
+        Login
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid spacing={2} container>
+          <Grid item xs={12} md={6}>
+            <TextField
+              label="Email"
+              type="email"
+              variant="outlined"
+              required
+              fullWidth
+              value={email}
+              onChange={e => setEmail(e.target.value.trim())}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              label="Password"
+              type="password"
+              variant="outlined"
+              required
+              fullWidth
+              value={password}
+              onChange={e => setPassword(e.target.value.trim())}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" fullWidth variant="contained" color="success">
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </>
   )
 }
 
