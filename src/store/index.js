6bd89bb8308339e396f34452c5f58d0fd2da1ae4
Liamm3 <liam.wohlfart0@gmail.com>
2021-11-16 10:38:14 +0100

    Add support redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import rootReducer from './rootReducer'
 import client from '../client'
 
-export default createStore(
-  rootReducer,
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const middleware = composeEnhancers(
   applyMiddleware(thunk.withExtraArgument({ client }))
 )
+
+export default createStore(rootReducer, middleware)
